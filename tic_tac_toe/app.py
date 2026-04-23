66ade09351ddd902c3663c4bdb034e67cd39df53
akky-cronix <akshay.an2930@gmail.com>
2026-04-24 00:00:20 +0530

    Update app.py

--- tic_tac_toe/app.py
+++ tic_tac_toe/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 import time
-from minimax import minimax, nodes_minimax
-from alphabeta import alphabeta, nodes_ab
+
+from minimax import minimax, nodes_minimax, reset_minimax
+from alphabeta import alphabeta, nodes_ab, reset_ab
 from utils import get_moves
 
 app = Flask(__name__)
 
+# ------------------ BEST MOVE (MINIMAX) ------------------
 def best_move_minimax(board):
-    global nodes_minimax
-    nodes_minimax = 0
     best_val = -1000
     move = -1
 
@@ -19,11 +19,12 @@ def best_move_minimax(board):
         if val > best_val:
             best_val = val
             move = i
+
     return move
 
+
+# ------------------ BEST MOVE (ALPHA-BETA) ------------------
 def best_move_ab(board):
-    global nodes_ab
-    nodes_ab = 0
     best_val = -1000
     move = -1
 
@@ -34,32 +35,44 @@ def best_move_ab(board):
         if val > best_val:
             best_val = val
             move = i
+
     return move
 
+
+# ------------------ ROUTES ------------------
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/move", methods=["POST"])
 def move():
     data = request.json
     board = data["board"]
 
+    # ---- MINIMAX ----
+    reset_minimax()
     start = time.time()
     m1 = best_move_minimax(board.copy())
     t1 = time.time() - start
+    nodes_m = nodes_minimax
 
+    # ---- ALPHA-BETA ----
+    reset_ab()
     start = time.time()
     m2 = best_move_ab(board.copy())
     t2 = time.time() - start
+    nodes_a = nodes_ab
 
     return jsonify({
-        "move": m2,
+        "move": m2,  # using Alpha-Beta move
         "minimax_time": t1,
         "alphabeta_time": t2,
-        "nodes_minimax": nodes_minimax,
-        "nodes_ab": nodes_ab
+        "nodes_minimax": nodes_m,
+        "nodes_ab": nodes_a
     })
 
+
+# ------------------ RUN APP ------------------
 if __name__ == "__main__":
     app.run(debug=True)
