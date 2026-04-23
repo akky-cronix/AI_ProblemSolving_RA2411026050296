cd3c469074699e8b60b5de6630b3d7531fbcfb5c
akky-cronix <akshay.an2930@gmail.com>
2026-04-23 23:37:25 +0530

    Add minimax function for game AI
    Implement minimax algorithm for Tic Tac Toe with win/draw checks.

--- tic_tac_toe/minimax.py
+++ tic_tac_toe/minimax.py
@@ -1,1 +1,30 @@
+from utils import check_winner, is_draw, get_moves
 
+nodes_minimax = 0
+
+def minimax(board, is_max):
+    global nodes_minimax
+    nodes_minimax += 1
+
+    winner = check_winner(board)
+    if winner == "O":
+        return 1
+    elif winner == "X":
+        return -1
+    elif is_draw(board):
+        return 0
+
+    if is_max:
+        best = -1000
+        for move in get_moves(board):
+            board[move] = "O"
+            best = max(best, minimax(board, False))
+            board[move] = " "
+        return best
+    else:
+        best = 1000
+        for move in get_moves(board):
+            board[move] = "X"
+            best = min(best, minimax(board, True))
+            board[move] = " "
+        return best
