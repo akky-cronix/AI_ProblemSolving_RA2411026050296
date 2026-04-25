d669b22b542fc6c539eb2ed3cd897ba05d2b0539
akky-cronix <akshay.an2930@gmail.com>
2026-04-25 10:41:17 +0530

    Added optimal move suggestion system

--- evacuation_planner/interactive.py
+++ evacuation_planner/interactive.py
@@ -1,1 +1,10 @@
+from bfs import bfs
 
+def suggest_next_move(current_state):
+    path = bfs()
+
+    for i in range(len(path)-1):
+        if path[i] == current_state:
+            return path[i+1]
+
+    return None
